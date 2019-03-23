f3703668621a0ac00155363a756f01b50a829592
Bohee Kim <qhgml9704@naver.com>
2019-03-24 01:36:05 +0900

    roll a ball

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+
+	private Vector3 offset;
+
+	void Start ()
+	{
+		offset = transform.position - player.transform.position; //카메라 좌표 - 플레이어 좌표
+	}
+
+	void LateUpdate ()
+	{
+		transform.position = player.transform.position + offset;
+	}
+}
